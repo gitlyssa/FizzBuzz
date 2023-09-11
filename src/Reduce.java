5f5f553da81da7ba88a8ec544ba796c6c21fb942
gitlyssa <121577615+gitlyssa@users.noreply.github.com>
2023-09-11 10:59:42 -0400

    almost done

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int steps = 0;
+        while (n != 0) {
+            boolean isEven = n % 2 == 0; // TODO: 2023-09-11
+
+            if (isEven) {
+                n /= 2;
+            }
+            else  {
+                n -= 1;
+            }
+        }
+    }
 }
